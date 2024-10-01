045415ae3c1c5a0d94c5d59262d93c63a8fe3e4e
alvarovianello <alvaro_s_o@hotmail.com>
2024-10-01 02:00:09 -0300

    Configure Terraform e CI/CD

--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "cluster_name" {
-  value = aws_eks_cluster.fastfood_cluster.name
-}
-
-output "node_group_name" {
-  value = aws_eks_node_group.fastfood_node_group.node_group_name
-}
---
+++ terraform/vars.tf
@@ -0,0 +1,27 @@
+variable "regionDefault" {
+    default = "us-east-1"
+}
+
+variable "projectName" {
+    default = "EKS-FastFood"
+}
+
+variable "labRole" {
+    default = "arn:aws:iam::659767469388:role/LabRole"
+}
+
+variable "accessConfig" {
+    default = "API_AND_CONFIG_MAP"
+}
+
+variable "nodeGrop" {
+    default = "FastFood"
+}
+
+variable "principalArn" {
+    default = "arn:aws:iam::659767469388:role/voclabs"
+}
+
+variable "policyArn" {
+    default = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+}
