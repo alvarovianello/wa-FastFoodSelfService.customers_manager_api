5f475629e9676e259e88658fc4303de819946822
alvarovianello <alvaro_s_o@hotmail.com>
2024-10-04 08:59:58 -0300

    Configuration Deploy AWS

--- terraform/eks-access-entry.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_eks_access_entry" "eks-access-entry-customer" {
-  cluster_name      = aws_eks_cluster.eks-cluster-customer.name
-  principal_arn     = var.principalArn
-  kubernetes_groups = ["customer-group-kubernetes"]
-  type              = "STANDARD"
-}
